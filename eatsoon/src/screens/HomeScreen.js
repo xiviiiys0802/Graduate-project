9a82f0d43c2dbf74db6e0d855dac3f6c1009223e
amsrj <knockinonhvns@gmail.com>
2025-07-16 23:44:47 +0900

    공통 스타일 파일 생성

--- eatsoon/src/screens/HomeScreen.js
+++ eatsoon/src/screens/HomeScreen.js
@@ -1,16 +1,5 @@
 import React from 'react';
-import styled from 'styled-components/native';
-
-const Container = styled.View`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Title = styled.Text`
-  font-size: 24px;
-  font-weight: bold;
-`;
+import { Container, Title } from '../components/StyledComponents';
 
 const HomeScreen = () => (
   <Container>
