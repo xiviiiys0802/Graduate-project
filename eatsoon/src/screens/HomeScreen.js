5ea5801f57d7d8c5401bf328dc0fe05b6fa9c6d7
amsrj <knockinonhvns@gmail.com>
2025-07-18 13:39:48 +0900

    주석 추가

--- eatsoon/src/screens/HomeScreen.js
+++ eatsoon/src/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Title } from '../components/StyledComponents';
 
+//홈 탭의 메인 화면 컴포넌트
 const HomeScreen = () => (
   <Container>
     <Title>홈 화면</Title>
