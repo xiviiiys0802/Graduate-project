3c159b8c505d42c11dee7d1316b8e965f3ddce84
printji <jounginyoon@naver.com>
2025-07-25 11:36:54 +0900

    expo go가 실행되길 바라며

--- eatsoon/App.js
+++ eatsoon/App.js
@@ -2,20 +2,9 @@
 
 import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-<<<<<<< Updated upstream
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import SignUpScreen from './src/screens/SignUpScreen';
-import LoginScreen from './src/screens/LoginScreen';
-import MainTabs from './src/navigations/MainTabs';
-import NotificationSettingsScreen from './src/screens/NotificationSettingsScreen'; // 경로 수정
-
-const Stack = createNativeStackNavigator();
-=======
 import TabNavigation from './src/navigations/TabNavigation';
 import Constants from 'expo-constants';
 import { registerForPushNotificationsAsync } from './src/utils/notification';
->>>>>>> Stashed changes
 
 export default function App() {
   useEffect(() => {
@@ -29,16 +18,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} />
-        <Stack.Screen name="MainTabs" component={MainTabs} />
-        <Stack.Screen
-          name="NotificationSettings"
-          component={NotificationSettingsScreen}
-          options={{ title: '알림 설정' }}
-        />
-      </Stack.Navigator>
+      <TabNavigation />
     </NavigationContainer>
   );
 }
