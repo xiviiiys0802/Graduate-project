169e965526ba706d41da1b9beb8077dee7c41c56
xiviiiys02 <xiviiiys02@gmail.com>
2025-08-07 00:14:04 +0900

    UI 분리: ProfileScreen과 MoreScreen 기능 구조 정리

--- eatsoon/src/navigations/SettingsStack.js
+++ eatsoon/src/navigations/SettingsStack.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SettingsScreen from '../screens/SettingsScreen';
+import MoreScreen from '../screens/MoreScreen';
 import RecipeRecommendationScreen from '../screens/RecipeRecommendationScreen';
 import StatisticsReportScreen from '../screens/StatisticsReportScreen';
 import ShoppingListScreen from '../screens/ShoppingListScreen';
@@ -13,7 +13,7 @@ export default function SettingsStack() {
     console.log('✅ SettingsStack loaded'); 
     return (
     <Stack.Navigator>
-      <Stack.Screen name="Settings" component={SettingsScreen} />
+      <Stack.Screen name="More" component={MoreScreen} />
       <Stack.Screen name="RecipeRecommendation" component={RecipeRecommendationScreen} />
       <Stack.Screen name="StatisticsReport" component={StatisticsReportScreen} />
       <Stack.Screen name="ShoppingList" component={ShoppingListScreen} />
